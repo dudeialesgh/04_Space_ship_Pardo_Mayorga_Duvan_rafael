685cb1e98cdf61d040b22dad918025e34e2610c5
dudeialesgh <rafael.pardo@fundacion-jala.org>
2023-06-13 01:04:19 -0500

    uno

---
+++ game/components/game.py
@@ -0,0 +1,57 @@
+import pygame
+
+from game.utils.constants import BG, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE, ICON
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(TITLE)
+        pygame.display.set_icon(ICON)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.playing = False
+        self.game_speed = 10
+
+        self.x_pos_bg = 0
+        self.y_pos_bg = 0
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+            pygame.display.update()
+        pygame.display.quit()
+        pygame.quit()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                pygame.quit()
+                quit()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.clock.tick(FPS)
+        self.screen.fill((255, 255, 255))
+        self.draw_background()
+        pygame.display.update()
+        pygame.display.flip()
+
+    def draw_background(self):
+        image = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        image_height = image.get_height()
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg-image_height))
+
+        if self.y_pos_bg >= SCREEN_HEIGHT:
+            self.screen.blit(
+                image, (self.x_pos_bg, self.y_pos_bg-image_height))
+            self.y_pos_bg = 0
+        self.y_pos_bg += self.game_speed
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from game.components.game import Game 
+
+
 if __name__ == "__main__":
-    print("Hello everyone!!!")
+    #print("Hello everyone!!!")
+    game = Game()
+    game.run()
     
