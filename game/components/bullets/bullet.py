9d5217e47dbd5f6b0e80de2d03010241f215e3fb
dudeialesgh <rafael.pardo@fundacion-jala.org>
2023-06-14 23:27:02 -0500

    Create bullet.py

---
+++ game/components/bullets/bullet.py
@@ -0,0 +1,31 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+from game.utils.constants import BULLET, BULLET_ENEMY, SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+class Bullet(Sprite):
+    X_POS = 80
+    Y_POS = 310
+    SPEED = 20
+    BULLET_SIZE = pygame.transform.scale(BULLET, (20, 20))
+    BULLET_SIZE_ENEMY = pygame.transform.scale(BULLET_ENEMY, (20, 20))
+    BULLETS = {'player': BULLET_SIZE, 'enemy': BULLET_SIZE_ENEMY}
+    def __init__(self, spaceship):
+        self.image = self.BULLETS[spaceship.type]
+        self.rect = self.image.get_rect()
+        self.rect.center = spaceship.rect.center
+        self.owner =   spaceship.type
+
+
+    def events(self):
+        pass
+
+    def update(self, bullets):
+        self.rect.y -= self.SPEED
+        if self.rect.y >= SCREEN_HEIGHT:
+            bullets.remove(self)
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
