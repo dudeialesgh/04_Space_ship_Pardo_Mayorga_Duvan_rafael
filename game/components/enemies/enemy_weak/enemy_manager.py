d48dbc5c4ddb4392f1835a54fe382d8957092541
dudeialesgh <rafael.pardo@fundacion-jala.org>
2023-06-14 23:27:10 -0500

    Update enemy_manager.py

--- game/components/enemies/enemy_weak/enemy_manager.py
+++ game/components/enemies/enemy_weak/enemy_manager.py
@@ -4,10 +4,11 @@ class EnemyManager:
     def __init__(self):
         self.enemies = []
 
-    def update(self):
+    def update(self, game):
         self.add_enemy()
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
+
 
     def add_enemy(self):
         if len(self.enemies) < 5:
