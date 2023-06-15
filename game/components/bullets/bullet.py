e2938e8da9f107ce64cefebe027897c1c1497293
dudeialesgh <rafael.pardo@fundacion-jala.org>
2023-06-15 00:31:04 -0500

    Update bullet.py

--- game/components/bullets/bullet.py
+++ game/components/bullets/bullet.py
@@ -9,8 +9,8 @@ class Bullet(Sprite):
     X_POS = 80
     Y_POS = 310
     SPEED = 20
-    BULLET_SIZE = pygame.transform.scale(BULLET, (20, 20))
-    BULLET_SIZE_ENEMY = pygame.transform.scale(BULLET_ENEMY, (20, 20))
+    BULLET_SIZE = pygame.transform.scale(BULLET, (10, 20))
+    BULLET_SIZE_ENEMY = pygame.transform.scale(BULLET_ENEMY, (9, 32))
     BULLETS = {'player': BULLET_SIZE, 'enemy': BULLET_SIZE_ENEMY}
     def __init__(self, spaceship):
         self.image = self.BULLETS[spaceship.type]
@@ -19,11 +19,12 @@ class Bullet(Sprite):
         self.owner =   spaceship.type
 
 
+
     def events(self):
         pass
 
     def update(self, bullets):
-        self.rect.y -= self.SPEED
+        self.rect.y += self.SPEED
         if self.rect.y >= SCREEN_HEIGHT:
             bullets.remove(self)
 
