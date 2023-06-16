05598f313927b50113d896eb6a6b96d1054f0b27
dudeialesgh <rafael.pardo@fundacion-jala.org>
2023-06-15 19:41:59 -0500

    Update game.py

--- game/components/game.py
+++ game/components/game.py
@@ -1,11 +1,13 @@
 import pygame
 
-from game.utils.constants import BG, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE, ICON
+from game.utils.constants import BG, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE, ICON, FONT_STYLE
 
 from game.components.spaceship import Spaceship
 from game.components.enemies.enemy_weak.enemy_manager import EnemyManager
 
 from game.components.bullets.bullet_manager import BulletManager
+
+from game.components.menu import Menu
 
 class Game:
     def __init__(self):
@@ -25,16 +27,27 @@ class Game:
         # 
         self.enemy_manager = EnemyManager()
         self.bullet_manager = BulletManager()
+        self.running = False
+        self.score = 0
+        self.death_count=0
+        self.menu = Menu('Press any key to start....',self.screen)
+
+    def execute(self):
+        self.running = True
+        while self.running:
+            if not self.playing:
+                self.show_menu()
+        pygame.display.quit()
+        pygame.quit()
 
     def run(self):
+        self.enemy_manager.reset()
+        self.score = 0
         self.playing = True
         while self.playing:
             self.events()
             self.update()
             self.draw()
-            pygame.display.update()
-        pygame.display.quit()
-        pygame.quit()
 
     def events(self):
         for event in pygame.event.get():
@@ -58,6 +71,7 @@ class Game:
         self.player.draw(self.screen)
         self.enemy_manager.draw(self.screen)
         self.bullet_manager.draw(self.screen)
+        self.draw_score()
         pygame.display.update()
         pygame.display.flip()
 
@@ -72,3 +86,31 @@ class Game:
                 image, (self.x_pos_bg, self.y_pos_bg-image_height))
             self.y_pos_bg = 0
         self.y_pos_bg += self.game_speed
+    
+    def show_menu(self):
+        self.menu.reset_screen_collor(self.screen)
+        half_screen_width = SCREEN_WIDTH // 2
+        half_screen_height = SCREEN_HEIGHT // 2
+    
+        if self.death_count == 0:
+            self.menu.draw(self.screen)
+        else:
+            self.menu.update_message('Press any key to start again...')
+            self.menu.draw(self.screen)
+
+        icon = pygame.transform.scale(ICON, (80,120))
+        self.screen.blit(icon, (half_screen_width-50, half_screen_height-150))
+
+        self.menu.update(self)
+
+    def update_score(self):
+        self.score += 1
+
+    def draw_score(self):
+        font = pygame.font.Font(FONT_STYLE, 30)
+        text = font.render(f'Score: {self.score}', True, (255,255,255))
+        text_rect = text.get_rect()
+        text_rect.center = (1000, 50)
+        self.screen.blit(text, text_rect)
+
+    
