4bcee69534f0cb86ae96e9372ed71eb6a85edfab
dudeialesgh <rafael.pardo@fundacion-jala.org>
2023-06-16 01:47:22 -0500

    Update menu.py

--- game/components/menu.py
+++ game/components/menu.py
@@ -5,14 +5,14 @@ from game.utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT, FONT_STYLE
 
 class Menu:
     HALF_SCREEN_WIDTH = SCREEN_WIDTH // 2
-    HALF_SCREEN_WIDTH = SCREEN_HEIGHT // 2
+    HALF_SCREEN_HEIGHT = SCREEN_HEIGHT // 2
 
     def __init__(self, message,screen):
         screen.fill((255,255,255))
         self.font = pygame.font.Font(FONT_STYLE, 30)
         self.text = self.font.render(message, True, (0,0,0))
         self.text_rect = self.text.get_rect()
-        self.text_rect.center = (self.HALF_SCREEN_WIDTH, self.HALF_SCREEN_WIDTH)
+        self.text_rect.center = (self.HALF_SCREEN_WIDTH, self.HALF_SCREEN_HEIGHT)
 
     def handle_events_to_menu(self,game):
         for event in pygame.event.get():
@@ -32,7 +32,7 @@ class Menu:
     def update_message(self,message):
         self.text = self.font.render(message, True, (0,0,0))
         self.text_rect = self.text.get_rect()
-        self.text_rect.center = (self.HALF_SCREEN_WIDTH, self.HALF_SCREEN_WIDTH)
+        self.text_rect.center = (self.HALF_SCREEN_WIDTH, self.HALF_SCREEN_HEIGHT)
 
     def reset_screen_collor(self,screen):
         screen.fill((255,255,255))
