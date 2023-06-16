99e714943f539226e4e6489afa43dd7d0258fe9b
dudeialesgh <rafael.pardo@fundacion-jala.org>
2023-06-16 01:47:21 -0500

    Update bullet_manager.py

--- game/components/bullets/bullet_manager.py
+++ game/components/bullets/bullet_manager.py
@@ -6,37 +6,36 @@ class BulletManager:
     def __init__(self):
         self.bullets = []
         self.enemy_bullets = []
-        self.ally_bullets = []
 
     def update(self, game):
         for bullet in self.enemy_bullets:
             bullet.update(self.enemy_bullets)
 
             if bullet.rect.colliderect(game.player.rect) and bullet.owner == 'enemy':
-                self.bullets.remove(bullet)
+                self.enemy_bullets.remove(bullet)
                 game.playing = False
                 game.death_count += 1
                 pygame.time.delay(5000)
                 break
         
-        for bullet in self.ally_bullets:
-            bullet.update(self.ally_bullets)
+        for bullet in self.bullets:
+            bullet.update(self.bullets)
             # condicional para verificar que el bullet colisione con el enemigo
             for enemy in game.enemy_manager.enemies:
                 if bullet.rect.colliderect(enemy.rect) and bullet.owner == 'player':
                     game.enemy_manager.enemies.remove(enemy)
-                    self.ally_bullets.remove(bullet)
+                    self.bullets.remove(bullet)
                     game.update_score()
                     
 
     def draw(self, screen):
         for bullet in self.enemy_bullets:
             bullet.draw(screen)
-        for bullet in self.ally_bullets:
+        for bullet in self.bullets:
             bullet.draw(screen)
 
     def add_bullet(self, bullet):
         if bullet.owner == 'enemy' and len(self.enemy_bullets) < 1:
             self.enemy_bullets.append(bullet)
-        elif bullet.owner == 'player' and len(self.ally_bullets) < 1:
-            self.ally_bullets.append(bullet)
+        elif bullet.owner == 'player' and len(self.bullets) < 1:
+            self.bullets.append(bullet)
